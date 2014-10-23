a5daa211b99b3ee0ff0f05508ea99e442315d747
Chad Reynoldson <chad@reynoldsoncontrol.com>
2014-10-23 07:31:19 -0500

    v3 dynamic styles

--- myApp.js
+++ myApp.js
@@ -6,15 +6,30 @@ angular.module('myApp', [])
         numbers[i] = i + 1;
       };
       return numbers;
-    }
+    };
 
     $scope.compute = function(a,b) {
       return a * b;
-    }
+    };
 
     $scope.$watch('numberLimit', function(limit) {
       $scope.numbers = populateNumbers(limit);
     });
 
     $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+    var activeFactorA, activeFactorB;
+    $scope.setActiveFactors = function(a, b) {
+        activeFactorA = a;
+        activeFactorB = b;
+    };
+
+    $scope.matchesFactor = function (a, b) {
+        return a === activeFactorA || b === activeFactorB;
+    };
+
+    $scope.clearActiveFactors = function() {
+      activeFactorA = activeFactorB = null;
+    };
+  
 });
